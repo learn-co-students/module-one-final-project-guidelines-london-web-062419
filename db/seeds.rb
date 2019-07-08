8ba944a0cb1495774a26a9dd225edf95a3823074
Anne Marie O'Sullivan <annemarieos.irl@gmail.com>
2019-07-08 22:50:38 +0100

    code refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,6 @@ company1 = Company.create(name: "Go Explore")
 company2 = Company.create(name: "Travel Buddy")
 company3 = Company.create(name: "World Traveller")
 
-customer1 = Customer.create(first_name: "Laura", last_name: "O'Sullivan", email_address: "laura.dent@gmail.com")
-customer2 = Customer.create(first_name: "Moose", last_name: "Loser", email_address: "loser@gmail.com")
-
 
 puts "----------------------------------------"
 puts "----------------------------------------"
@@ -14,8 +11,7 @@ puts "------------- SEEDS RAN  ---------------"
 puts "----------------------------------------"
 puts "----------------------------------------"
 
-trip1 = Trip.create(name: "Toph's big trip", start_date: 24/9/19, end_date: 25/9/19, price: 450, destination: DESTINATIONS.sample, customer_id: customer1.id.to_i, company_id: company1.id.to_i)
-trip2 = Trip.create(name: "Lads Trip", start_date: 29/9/19, end_date: 31/10/19, price: 780, destination: DESTINATIONS.sample, customer_id: customer2.id.to_i, company_id: company2.id.to_i)
+
 
 
 
--- lib/customer.rb
+++ lib/customer.rb
@@ -3,11 +3,10 @@ class Customer < ActiveRecord::Base
     has_many :trips
 
     def view_customers_trips
-       if Trip.all.select {|trip| trip.customer_id == nil}
+       if self.trips.empty?
         puts "You have no trips!"
-       else 
-        trips =  Trip.all.select {|trip| trip.customer_id == self.id}
-        trips.map {|trip| "
+       else
+        self.trips.map {|trip| "
          Trip Name: #{trip.name},
          Start Date: #{trip.start_date},
          End Date: #{trip.end_date}, 
